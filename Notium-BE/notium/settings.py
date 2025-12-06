87cd214c2eac3fa1ff7ba980f5f3e9ee56d1cb1d
NiteshBabu <n.babu@clerkswell.com>
2025-12-06 21:56:30 +0530

    chore: whitelist host to deploy on render

--- Notium-BE/notium/settings.py
+++ Notium-BE/notium/settings.py
@@ -10,9 +10,8 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
-from pathlib import Path
 import os
-
+from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -22,12 +21,14 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv("SECRET_KEY", "django-insecure-^o(qxu9*ty^3j8i2hijue5q3cr9s(0cl&^afud+##fe61%z0(o")
+SECRET_KEY = os.getenv(
+    "SECRET_KEY", "django-insecure-^o(qxu9*ty^3j8i2hijue5q3cr9s(0cl&^afud+##fe61%z0(o"
+)
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["localhost", "127.0.0.1"]
+ALLOWED_HOSTS = ["localhost", "127.0.0.1", "notium.onrender.com"]
 
 
 INSTALLED_APPS = [
@@ -37,8 +38,8 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    'corsheaders',
-    "notes"
+    "corsheaders",
+    "notes",
 ]
 
 MIDDLEWARE = [
